a74223671273333e94dbcb44784f429292367dd6
ArkinDharawat <adharawat@umass.edu>
2021-10-27 21:58:43 +0530

    Added notebook and convert.py. Added fixed json

---
+++ convert.py
@@ -0,0 +1,31 @@
+import srsly
+import typer
+import warnings
+from pathlib import Path
+
+import spacy
+from spacy.tokens import DocBin
+
+
+def convert(lang: str, input_path: Path, output_path: Path):
+    nlp = spacy.blank(lang)
+    db = DocBin()
+    # print(srsly.read_json(input_path)[0])
+    for text, annot in srsly.read_json(input_path):
+        print(text)
+        doc = nlp.make_doc(text)
+        ents = []
+        for start, end, label in annot["entities"]:
+            span = doc.char_span(start, end, label=label)
+            if span is None:
+                msg = f"Skipping entity [{start}, {end}, {label}] in the following text because the character span '{doc.text[start:end]}' does not align with token boundaries:\n\n{repr(text)}\n"
+                warnings.warn(msg)
+            else:
+                ents.append(span)
+        doc.ents = ents
+        db.add(doc)
+    db.to_disk(output_path)
+
+
+if __name__ == "__main__":
+    typer.run(convert)
